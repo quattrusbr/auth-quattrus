7789214d6054afd56721fde0347214c8b330157f
Carol Ferraz <fferraz.carol@gmail.com>
2024-03-05 17:01:33 -0300

    feat: conect API to front

--- src/app/(dashboard)/tarefas/add-tasks.tsx
+++ src/app/(dashboard)/tarefas/add-tasks.tsx
@@ -28,55 +28,120 @@ import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
 
-// Definindo um tipo para os itens de dados
-type TasksData = {
-  prioridade_task?: string;
-  oque_task: string;
-  porque_task: string;
-  como_task: string;
-  quem_task: string;
-  data_inicial?: string;
-  data_final?: string;
-  valor_task?: string;
-};
-
 import { Task } from "@/types/types";
 import { Button } from "@/app/components/button";
 import { DeleteButtonRow } from "./delete-button-row";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-const names = [
-  { value: "Carol" },
-  { value: "Ismael" },
-  { value: "Mano" },
-  { value: "Yan" },
-];
+const names = [{ value: 1430 }];
+
+interface IFormInputs {
+  prioridade: string;
+  oque: string;
+  porque: string;
+  comoOnde: string;
+  idUsuarioQuem: number;
+  dt_de: string;
+  dt_ate: string;
+  valor: number;
+  concluido: number;
+}
 
 export const AddTasks = ({ tasks }: { tasks: Task[] }) => {
   const [openDrawer, setOpenDrawer] = useState(false);
 
-  interface IFormInputs {
-    prioridade_task?: string;
-    oque_task: string;
-    porque_task: string;
-    como_task: string;
-    quem_task: string;
-    data_inicial?: string;
-    data_final?: string;
-    valor_task?: string;
-  }
-
   const { handleSubmit, control, reset, register } = useForm<IFormInputs>({
     defaultValues: {
-      oque_task: "",
-      porque_task: "",
-      como_task: "",
-      quem_task: "",
+      prioridade: "",
+      oque: "",
+      porque: "",
+      comoOnde: "",
+      idUsuarioQuem: 0,
+      dt_de: "",
+      dt_ate: "",
+      valor: 0,
+      concluido: 0,
     },
   });
-  const onSubmit: SubmitHandler<IFormInputs> = (data) => {
-    console.log(data);
-    console.log(dayjs(data.data_inicial).format("DD/MM/YYYY")); //formatar datas dessa forma para enviar para o backend
+
+  // const onSubmit: SubmitHandler<IFormInputs> = async (data) => {
+  //   try {
+  //     data.dt_de = dayjs(data.dt_de).format("DD/MM/YYYY");
+  //     data.dt_ate = dayjs(data.dt_ate).format("DD/MM/YYYY");
+  //     console.log(data);
+  //     console.log( JSON.stringify({ jsonData: [{ data }] }));
+
+  //     const response = await fetch(
+  //       "http://localhost:45272/Servicos/cadTarefas.asmx/CreateTarefas?idPK=&idReuniao=&abertas=true&concluidas=false&vencidas=tru",
+  //       {
+  //         method: "POST",
+  //         mode: "no-cors",
+  //         headers: {
+  //           "ASP.NET_SessionId": "i2fwmyjidh5wbviihis11yti",
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({ jsonData: [{ data }] }),
+  //       }
+  //     );
+
+  //     if (response.ok) {
+  //       const newTask = await response.json();
+  //       reset();
+  //       toggleDrawer();
+  //     } else {
+  //       console.error("Erro ao adicionar tarefa1:", response.statusText);
+  //     }
+  //   } catch (error) {
+  //     console.error("Erro ao adicionar tarefa:", error);
+  //   }
+  // };
+
+  const jsonData = {
+    jsonData: [
+      {
+        prioridade: 1,
+        oque: "teste do postman",
+        porque: "fsadsas",
+        comoOnde: "dsads",
+        idUsuarioQuem: 1430,
+        dt_de: "05/03/2024", // A data formatada corretamente
+        dt_ate: "29/03/2024", // A data formatada corretamente
+        valor: 32432,
+        concluido: 0,
+        dt_concluido: null,
+      },
+    ],
+  };
+
+  const onSubmit: SubmitHandler<IFormInputs> = async (data) => {
+    try {
+      data.dt_de = dayjs(data.dt_de).format("DD/MM/YYYY");
+      data.dt_ate = dayjs(data.dt_ate).format("DD/MM/YYYY");
+      console.log(data);
+      
+      const response = await fetch(
+        "http://localhost:4272/Servicos/cadTarefas.asmx/CreateTarefas?IdPK=&idReuniao=&abertas=true&concluidas=false&vencidas=true",
+        {
+          method: "POST",
+          mode: "no-cors",
+          headers: {
+            "ASP.NET_SessionId": "i2fwmyjidh5wbviihis11yti",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(jsonData),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("erro");
+      }
+
+      const result = await response.json();
+      console.log(result);
+      // Faça algo com os dados recebidos
+    } catch (e) {
+      console.error("Houve um problema com a operação fetch: ");
+    }
   };
 
   const toggleDrawer = () => {
@@ -181,101 +246,69 @@ export const AddTasks = ({ tasks }: { tasks: Task[] }) => {
             onSubmit={handleSubmit(onSubmit)}
           >
             <FormControl fullWidth sx={{ marginBottom: 2 }}>
-              <label id="prioridade_task">Prioridade</label>
+              <label id="prioridade">Prioridade</label>
               <TextField
                 inputProps={{
                   style: {
                     padding: 5,
                   },
                 }}
-                id="prioridade_task"
+                id="prioridade"
                 variant="filled"
                 type="number"
-                {...register("prioridade_task")}
+                {...register("prioridade")}
               />
             </FormControl>
 
-            <Controller
-              control={control}
-              name="data_inicial"
-              render={({ field }) => (
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    value={field.value}
-                    onChange={(value) => {
-                      field.onChange(value);
-                    }}
-                    format="YYYY-MM-DD"
-                    label={"data inicial"}
-                  />
-                </LocalizationProvider>
-              )}
-            />
-            <Controller
-              control={control}
-              name="data_final"
-              render={({ field }) => (
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    value={field.value}
-                    onChange={(value) => {
-                      field.onChange(value);
-                    }}
-                    format="YYYY-MM-DD"
-                    label={"data final"}
-                  />
-                </LocalizationProvider>
-              )}
-            />
             <FormControl fullWidth sx={{ marginBottom: 2 }}>
-              <label id="oque_task">O que</label>
+              <label id="oque">O que</label>
               <TextField
                 inputProps={{
                   style: {
                     padding: 5,
                   },
                 }}
-                id="oque_task"
+                id="oque"
                 variant="filled"
                 multiline
-                {...register("oque_task")}
+                {...register("oque")}
               />
             </FormControl>
             <FormControl fullWidth sx={{ marginBottom: 2 }}>
-              <label id="porque_task">Por quê?</label>
+              <label id="porque">Por quê?</label>
               <TextField
                 inputProps={{
                   style: {
                     padding: 5,
                   },
                 }}
-                id="porque_task"
+                id="porque"
                 variant="filled"
                 multiline
-                {...register("porque_task")}
+                {...register("porque")}
               />
-              <label id="como_task">Como/Onde</label>
+              <label id="comoOnde">Como/Onde</label>
               <TextField
                 inputProps={{
                   style: {
                     padding: 5,
                   },
                 }}
-                id="como_task"
+                id="comoOnde"
                 variant="filled"
                 multiline
-                {...register("como_task")}
+                {...register("comoOnde")}
               />
-              <label id="quem_task">Quem</label>
+              <label id="idUsuarioQuem">Quem</label>
               <Select
                 inputProps={{
                   style: {
                     padding: 5,
                   },
                 }}
-                id="quem_task"
+                id="idUsuarioQuem"
                 variant="filled"
-                {...register("quem_task")}
+                {...register("idUsuarioQuem")}
               >
                 {names.map((option) => (
                   <MenuItem key={option.value} value={option.value}>
@@ -283,6 +316,51 @@ export const AddTasks = ({ tasks }: { tasks: Task[] }) => {
                   </MenuItem>
                 ))}
               </Select>
+              <label id="dt_de">Data Inicial</label>
+              <Controller
+                control={control}
+                name="dt_de"
+                render={({ field }) => (
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      value={field.value}
+                      onChange={(value) => {
+                        field.onChange(value);
+                      }}
+                      format="YYYY-MM-DD"
+                      label={"data inicial"}
+                    />
+                  </LocalizationProvider>
+                )}
+              />
+              <label id="dt_ate">Data Final</label>
+              <Controller
+                control={control}
+                name="dt_ate"
+                render={({ field }) => (
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      value={field.value}
+                      onChange={(value) => {
+                        field.onChange(value);
+                      }}
+                      format="YYYY-MM-DD"
+                    />
+                  </LocalizationProvider>
+                )}
+              />
+              <label id="valor">Valor</label>
+              <TextField
+                inputProps={{
+                  style: {
+                    padding: 5,
+                  },
+                }}
+                id="valor"
+                variant="filled"
+                multiline
+                {...register("valor")}
+              />
             </FormControl>
             <div className="flex">
               <Button
@@ -293,7 +371,7 @@ export const AddTasks = ({ tasks }: { tasks: Task[] }) => {
                 Cancelar
               </Button>
               <Button
-                className="bg-primaryMain px-3 py-2 rounded-lg"
+                sx={{ backgroundColor: "primary.main", color: "common.white" }}
                 type="submit"
               >
                 Salvar
